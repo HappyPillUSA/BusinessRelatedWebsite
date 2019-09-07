96186097d026ee63af1164a12496bfea02cb3b02
userq11 <unlimited5511@interia.pl>
2019-09-07 13:53:15 +0200

    video toggle button - js

--- js/app.js
+++ js/app.js
@@ -6,4 +6,17 @@ $(document).ready(function() {
    $(".navBtn").click(function() {
       $(".nav").toggleClass("nav-show");
    });
+
+   // video switch
+   $(".video-switch-container").click(function() {
+      const value = $(".video-switch-btn").hasClass("btnSlide");
+      if(value) {
+         $(".video-switch-btn").removeClass("btnSlide");
+         $("#video").get(0).play();
+      }
+      else {
+         $(".video-switch-btn").addClass("btnSlide");
+         $("#video").get(0).pause();
+      }
+   });
 });
